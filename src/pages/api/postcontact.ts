9a61efba145ec48ac8556bee1ff363dac7d8cd22
shivani maiyani <shivani.maiyani@scaletech.xyz>
2023-09-01 08:26:15 +0530

    design contact form and integrated add contact api

--- src/pages/api/postcontact.ts
+++ src/pages/api/postcontact.ts
@@ -1,16 +1,23 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
 import * as fs from 'node:fs';
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
     if (req.method === "POST") {
+        try {
+            const data = await fs.promises.readdir('contactdata');
 
-        fs.writeFile('contactdata/1.json', JSON.stringify(req.body), () => { })
-        res.status(200).json(req)
+            const fileName = `contactdata/${data.length + 1}.json`;
+            await fs.promises.writeFile(fileName, JSON.stringify(req.body));
 
+            res.status(200).json({ message: "Data saved successfully." });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "An error occurred while saving the data." });
+        }
     } else {
-        res.status(200).json(['allblogs'])
+        res.status(200).json(['allblogs']);
     }
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,131 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+import Styles from "../styles/Contact.module.css";
 
 const Contact = () => {
-    return <div>This is contact page.</div>;
+    const initialValues = {
+        name: "",
+        email: "",
+        phone: "",
+        desc: "",
+    };
+
+    const onSubmit = async (values: any, { resetForm }: any) => {
+        console.log(values);
+        try {
+            const response = await fetch(
+                "http://localhost:3001/api/postcontact",
+                {
+                    method: "POST", // or 'PUT'
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(values),
+                }
+            );
+
+            const result = await response.json();
+            resetForm();
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    return (
+        <div>
+            <h1 className={Styles.title}>Contact us</h1>
+            <Formik
+                initialValues={initialValues}
+                // validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                <Form>
+                    <div className={Styles.formfield}>
+                        <div className={Styles.fieldinput}>
+                            <label className="form-label" htmlFor="name">
+                                Name
+                            </label>
+                            <Field
+                                type="text"
+                                placeholder="Enter name"
+                                className="form-control"
+                                id="exampleInputName"
+                                name="name"
+                            />
+                        </div>
+                        <ErrorMessage
+                            name="name"
+                            component="div"
+                            className={Styles.errormessage}
+                        />
+                    </div>
+                    <div className={Styles.formfield}>
+                        <div className={Styles.fieldinput}>
+                            <label className="form-label" htmlFor="email">
+                                Email
+                            </label>
+                            <Field
+                                type="email"
+                                placeholder="Enter email"
+                                className="form-control"
+                                id="exampleInputEmail1"
+                                name="email"
+                            />
+                        </div>
+                        <ErrorMessage
+                            name="email"
+                            component="div"
+                            className={Styles.errormessage}
+                        />
+                    </div>
+                    <div className={Styles.formfield}>
+                        <div className={Styles.fieldinput}>
+                            <label className="form-label" htmlFor="phone">
+                                Phone no
+                            </label>
+                            <Field
+                                type="text"
+                                placeholder="Enter phone"
+                                className="form-control"
+                                id="exampleInputPhone"
+                                name="phone"
+                            />
+                        </div>
+                        <ErrorMessage
+                            name="phone"
+                            component="div"
+                            className={Styles.errormessage}
+                        />
+                    </div>
+                    <div className={Styles.formfield}>
+                        <div className={Styles.fieldinput}>
+                            <label className="form-label" htmlFor="desc">
+                                Description
+                            </label>
+                            <Field
+                                as="textarea"
+                                placeholder="Enter description here..."
+                                id="desc"
+                                className="form-control"
+                                name="desc"
+                            />
+                        </div>
+                        <ErrorMessage
+                            name="desc"
+                            component="div"
+                            className={Styles.errormessage}
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                </Form>
+            </Formik>
+        </div>
+    );
 };
 
 export default Contact;
